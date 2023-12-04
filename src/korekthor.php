acc9f6f9ea990904feed45e36be52eb2be60ffff
TenViki <viktorkomarek28@gmail.com>
2023-12-04 19:55:59 +0100

    :wrench: Changes conf

---
+++ src/korekthor.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @package korekthor
+ */
+/*
+Plugin Name: Korekthor pro WordPress
+Plugin URI: https://korekthor.cz/
+Description: Korekthor plugin pro WordPress - integrujte sílu korekthoru přímo do vašich stránek!
+Version: 1.0.0
+Author: Viktor Komárek
+Author URI: https://vikithedev.eu/
+License: GPLv2 or later
+*/  
+
+// TODO: Add license
+
+// if (!defined("ABSPATH"))  die
