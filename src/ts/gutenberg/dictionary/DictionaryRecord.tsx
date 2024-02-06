5d6ace87d4197f568384d1322937e5c650b9f488
TenViki <viktorkomarek28@gmail.com>
2024-02-06 22:53:27 +0100

    :bug: Fixes import

--- src/ts/gutenberg/dictionary/DictionaryRecord.tsx
+++ src/ts/gutenberg/dictionary/DictionaryRecord.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-
-import { Dictionary } from "./DictionarySelect";
+import { Dictionary } from "../../../types";
 
 interface DictionaryRecordProps {
   dictionary: Dictionary;
@@ -8,17 +7,9 @@ interface DictionaryRecordProps {
   enabled: boolean;
 }
 
-const DictionaryRecord: React.FC<DictionaryRecordProps> = ({
-  dictionary,
-  onChange,
-  enabled,
-}) => {
+const DictionaryRecord: React.FC<DictionaryRecordProps> = ({ dictionary, onChange, enabled }) => {
   return (
-    <label
-      key={dictionary.id}
-      className="korekthor-dictionary-item"
-      htmlFor={"korekthor-" + dictionary.id}
-    >
+    <label key={dictionary.id} className="korekthor-dictionary-item" htmlFor={"korekthor-" + dictionary.id}>
       <div className="korekthor-dictionary-item-header">
         <input
           type="checkbox"
@@ -31,9 +22,7 @@ const DictionaryRecord: React.FC<DictionaryRecordProps> = ({
       </div>
 
       {dictionary.categories.length > 0 && (
-        <div className="korekthor-dictionary-item-categories">
-          {dictionary.categories.join(", ")}
-        </div>
+        <div className="korekthor-dictionary-item-categories">{dictionary.categories.join(", ")}</div>
       )}
 
       <p>{dictionary.description}</p>
